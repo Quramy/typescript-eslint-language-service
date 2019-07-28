d50c08e12e7fdacba6dd13e1b7e9872909865250
Yosuke Kurami <yosuke.kurami@gmail.com>
2019-07-28 20:14:49 +0900

    adapt fixer

--- src/eslint-adapter.test.ts
+++ src/eslint-adapter.test.ts
@@ -4,6 +4,7 @@ import { AstConverter } from "./ast-converter";
 import { ESLintAdapter } from "./eslint-adapter";
 import { ConfigProvider } from "./eslint-config-provider";
 import { ConfigArray } from "eslint/lib/cli-engine/config-array/config-array";
+import { TS_LANGSERVICE_ESLINT_DIAGNOSTIC_ERROR_CODE } from "./consts";
 
 class TestingConfigProvider implements ConfigProvider {
   public conf: any = {
@@ -41,6 +42,54 @@ describe("ESLintAdapter", () => {
       });
       const diagnostics = filterSourceFileFromDiagnosticList(adapter.getSemanticDiagnostics(() => [], "main.ts"));
       expect(diagnostics).toMatchSnapshot();
+    });
+  });
+
+  describe("#getCodeFixesAtPosition", () => {
+
+    it("shuld only delegate when input errorCodes does not include TS_LANGSERVICE_ESLINT_DIAGNOSTIC_ERROR_CODE", () => {
+      const configProvider = new TestingConfigProvider();
+      configProvider.conf.rules = {
+        quotes: [2, "double"],
+      };
+      const adapter = new ESLintAdapter({
+        converter: new AstConverter(),
+        getSourceFile: () => ts.createSourceFile("main.ts", "'use strict';", ts.ScriptTarget.ESNext, true),
+        configProvider,
+        logger: () => { },
+      });
+      const codeFixes = adapter.getCodeFixesAtPosition(() => [], "main.ts", 0, 12, [], { }, { });
+      expect(codeFixes).toMatchSnapshot();
+    });
+
+    it("shuld only delegate when input range isn't intersected ESLint reported results", () => {
+      const configProvider = new TestingConfigProvider();
+      configProvider.conf.rules = {
+        quotes: [2, "double"],
+      };
+      const adapter = new ESLintAdapter({
+        converter: new AstConverter(),
+        getSourceFile: () => ts.createSourceFile("main.ts", "'use strict';\n\nconst hoge = 1;", ts.ScriptTarget.ESNext, true),
+        configProvider,
+        logger: () => { },
+      });
+      const codeFixes = adapter.getCodeFixesAtPosition(() => [], "main.ts", 17, 30, [TS_LANGSERVICE_ESLINT_DIAGNOSTIC_ERROR_CODE], { }, { });
+      expect(codeFixes).toMatchSnapshot();
+    });
+
+    it("shuld return ESLint sourceCodeFixer result as TypeScript codeFixAction format", () => {
+      const configProvider = new TestingConfigProvider();
+      configProvider.conf.rules = {
+        quotes: [2, "double"],
+      };
+      const adapter = new ESLintAdapter({
+        converter: new AstConverter(),
+        getSourceFile: () => ts.createSourceFile("main.ts", "'use strict';", ts.ScriptTarget.ESNext, true),
+        configProvider,
+        logger: () => { },
+      });
+      const codeFixes = adapter.getCodeFixesAtPosition(() => [], "main.ts", 0, 12, [TS_LANGSERVICE_ESLINT_DIAGNOSTIC_ERROR_CODE], { }, { });
+      expect(codeFixes).toMatchSnapshot();
     });
   });
 });
